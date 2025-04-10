e89b030c804472da31b45418615627c3d687910a
Liudmila Joukovskaya <liudmila.joukovskaya@gmail.com>
2025-04-10 00:50:24 +0000

    add websockets

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -8,10 +8,59 @@ export class LlmAgent extends DurableObject<Env> {
 	async sayHello(name: string): Promise<string> {
 		return `Hello, ${name}!`;
 	}
+
+	async fetch(request: Request): Promise<Response> {
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+		this.ctx.acceptWebSocket(server);
+		return new Response(null, {
+		  status: 101,
+		  webSocket: client,
+		});
+	  }
+
+	  async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string) {
+		ws.send(
+		  `[Durable Object] message: ${message}, connections: ${this.ctx.getWebSockets().length}`,
+		);
+	  }
+
+	  async webSocketClose(
+		ws: WebSocket,
+		code: number,
+		reason: string,
+		wasClean: boolean,
+	  ) {
+		ws.close(code, "Durable Object is closing WebSocket");
+	  }
 }
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		if (request.method === "GET" && request.url.endsWith("/websocket")) {
+			// Expect to receive a WebSocket Upgrade request.
+			// If there is one, accept the request and return a WebSocket Response.
+			const upgradeHeader = request.headers.get("Upgrade");
+			if (!upgradeHeader || upgradeHeader !== "websocket") {
+			  return new Response(null, {
+				status: 426,
+				statusText: "Durable Object expected Upgrade: websocket",
+				headers: {
+				  "Content-Type": "text/plain",
+				},
+			  });
+			}
+
+			// This example will refer to a single Durable Object instance, since the name "foo" is
+			// hardcoded
+			let id = env.LLM_AGENT_DURABLE_OBJECT.idFromName("foo");
+			let stub = env.LLM_AGENT_DURABLE_OBJECT.get(id);
+
+			// The Durable Object's fetch handler will accept the server side connection and return
+			// the client
+			return stub.fetch(request);
+		  }
+
 		const id: DurableObjectId = env.LLM_AGENT_DURABLE_OBJECT.idFromName("foo");
 		const stub = env.LLM_AGENT_DURABLE_OBJECT.get(id);
 		const greeting = await stub.sayHello("world");
